d4591f3d2e4489b558b48d671b9ada26b5147bdd
Matthew McClain <MatthewMcClain17@users.noreply.github.com>
2016-04-27 16:49:27 -0700

    Added files via upload

---
+++ InfraredDronePeripheralControl.ino
@@ -0,0 +1,67 @@
+/* Infrared Drone Peripheral Control
+ * Allows an Apple infrared remote to control custom peripheral functions
+ * on an Oakwood Robotics Club drone or SalgaDrone.
+ * coded by Matthew McClain
+ * Version 0.1
+ * Copyright 2016 Oakwood Robotics Club
+ * 
+ * SETUP:
+ *  Connect IR Reciever to pin 11
+ *  Connect motor to pin ?
+ *  
+ *  VERSION HISTORY:
+ *  
+ *  0.1:
+ *    + pin variable declarations and IR codes
+ *    + irrecv and decode_results objects
+ *    + setup of basic IR sensing commands
+ *    + switch statement to perform a specific action based on code recieved
+ *    + created dropConfetti() and added testing code
+ */
+
+#include <IRremote.h>
+
+// VARIABLES
+// Pins
+const int RECV_PIN = 11; // IR reciever data pin
+const int ledPin = 13; // Onboard LED for testing
+
+// Remote Button Hexadecimal IR Codes
+const int playButton = 0x77E120D8;
+const int plusButton = 0x77E1D0D8;
+const int minusButton = 0x77E1B0D8;
+const int rewindButton = 0x77E110D8;
+const int forwardButton = 0x77E1E0D8;
+const int menuButton = 0x77E140D8;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup() {
+  irrecv.enableIRIn();
+  pinMode(ledPin, OUTPUT); 
+}
+
+void loop() {
+  if (irrecv.decode(&results)) { // if IR reciever recieves data
+    switch (results.value) { // determine button pressed from code recieved
+      case playButton: dropConfetti(); break;
+      case plusButton: break;
+      case minusButton: break;
+      case rewindButton: break;
+      case forwardButton: break;
+      case menuButton: break;
+    }
+  }
+}
+
+void dropConfetti() { // Drop confetti from the confetti module
+  // To do: write final code
+  // For testing purposes: flash the LED five times
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);
+  }
+}

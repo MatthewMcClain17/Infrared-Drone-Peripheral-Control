4c9d1771dc67b07ee573fd100b8cc476cfbf746f
Matthew McClain <MatthewMcClain17@students.oakwoodschool.org>
2016-04-27 17:03:55 -0700

    Changed name so it can run directly from its GitHub Desktop folder.

--- Infrared-Drone-Peripheral-Control.ino
+++ Infrared-Drone-Peripheral-Control.ino
@@ -5,6 +5,20 @@
  * Version 0.1
  * Copyright 2016 Oakwood Robotics Club
  * 
+ * * * * * IMPORTANT LIBRARY INFORMATION: * * * * *
+ * This code REQUIRES the use of the IRremote library, which can be found here:
+ * <https://github.com/z3t0/Arduino-IRremote>
+ * 
+ * FOR THIS LIBRARY ONLY, A PREEXISTING ARDUINO LIBRARY MUST BE DELETED. MAKE
+ * SURE TO SEE STEP 5 OF THE INSTALLATION INSTRUCTIONS ON THE LIBRARY'S README
+ * ON THE GITHUB PAGE.
+ * 
+ * For additional information on installing libraries, look here:
+ * <https://www.arduino.cc/en/Guide/Libraries>
+ * 
+ * For more information on how to make use of the IR library, look here:
+ * <https://learn.sparkfun.com/tutorials/ir-communication>
+ * 
  * SETUP:
  *  Connect IR Reciever to pin 11
  *  Connect motor to pin 10
